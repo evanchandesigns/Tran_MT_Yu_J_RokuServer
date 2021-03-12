9b98c1e1d52a00d9689ffd34dfb0d3f509bf69ec
evan chan <evanchan52.designs@gmail.com>
2021-03-11 23:53:45 -0500

    first

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,9 @@ const express = require('express');
 // like a traffic cop
 const router = express.Router();
 
+// import the sql connection
+const connect = require("../config/sqlConfig");
+
 // think of route handlers like PHP functions
 router.get("/", (req, res) => {
     //res.json = echo json_encode(....) in PHP
@@ -17,21 +20,39 @@ router.get("/users", (req, res) => {
     res.json({ message: "All users route" });
 })
 
-router.get("/movies", (req, res) => {
+router.get("/items", (req, res) => {
     // run a SQL query here -> get all movies from my DB
-    // res.json(query result here)
-    // echo a msg -> just like PHP
-    res.json({ message: "All movies route" });
+
+    connect.getConnection(function (err, connection) {
+        if (err) throw err; // not connected!
+
+        // Use the connection
+        connection.query('SELECT * FROM tbl_items', function (error, results) {
+            // When done with the connection, release it.
+            connection.release();
+
+            // Handle error after the release.
+            if (error) throw error;
+
+            // res.json(query result here)
+            // echo a msg -> just like PHP
+            res.json(results);
+
+            // Don't use the connection here, it has been returned to the pool.
+        });
+    });
 })
 
 // dynamic route handler that accept a parameter
 // this is like $_GET["id"] : (req.params.id)
 // you're passing the id via the route: /api/movies/1, /api/movies/20, etc
-router.get("/movies/:id", (req, res) => {
+router.get("/items/:id", (req, res) => {
     // run a SQL query here -> get all movies from my DB
-    // res.json(query result here)
-    // echo a msg -> just like PHP
-    res.json({ message: "Get 1 movie route", movie: req.params.id });
+    connect.query(`SELECT * FROM tbl_movies WHERE items_id=${req.params.id}`, function (error, results, fields) {
+
+        if (error) throw error;
+        res.json(results);
+    });
 })
 
 module.exports = router;
