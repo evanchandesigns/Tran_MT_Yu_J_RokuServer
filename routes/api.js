6a252d02a364817c27944ddcbc5e98fac0c1cc31
evan chan <evanchan52.designs@gmail.com>
2021-04-13 23:04:02 -0400

    settled

--- routes/api.js
+++ routes/api.js
@@ -7,20 +7,7 @@ router.get("/", (req, res) => {
     res.json({ message: "The main interface of the Roku App - Movies Section" });
 });
 
-router.get("/users", (req, res) => {
-    connect.getConnection(function (err, connection) {
-        if (err) throw err;
-
-        connection.query('SELECT * FROM tbl_user', function (error, results) {
-            connection.release();
-
-            if (error) throw error;
-            res.json(results);
-        });
-    });
-})
-
-router.get("/movies-parents", (req, res) => {
+router.get("/parents/movies", (req, res) => {
     connect.getConnection(function (err, connection) {
         if (err) throw err;
 
@@ -34,22 +21,8 @@ router.get("/movies-parents", (req, res) => {
 
 })
 
-router.get("/movies-kids", (req, res) => {
-    connect.getConnection(function (err, connection) {
-        if (err) throw err;
 
-        connection.query("SELECT * FROM tbl_movies WHERE movies_section = 'kids'", function (error, results) {
-            connection.release();
-
-            if (error) throw error;
-            res.json(results);
-        });
-    });
-
-})
-
-
-router.get("/tvs-parents", (req, res) => {
+router.get("/parents/tvs", (req, res) => {
     connect.getConnection(function (err, connection) {
         if (err) throw err;
 
@@ -63,20 +36,7 @@ router.get("/tvs-parents", (req, res) => {
 })
 
 
-router.get("/tvs-kids", (req, res) => {
-    connect.getConnection(function (err, connection) {
-        if (err) throw err;
-
-        connection.query("SELECT * FROM tbl_tv WHERE tv_section = 'kids'", function (error, results) {
-            connection.release();
-
-            if (error) throw error;
-            res.json(results);
-        });
-    });
-})
-
-router.get("/music-parents", (req, res) => {
+router.get("/parents/music", (req, res) => {
     connect.getConnection(function (err, connection) {
         if (err) throw err;
 
@@ -89,11 +49,11 @@ router.get("/music-parents", (req, res) => {
     });
 })
 
-router.get("/music-kids", (req, res) => {
+router.get("/kids", (req, res) => {
     connect.getConnection(function (err, connection) {
         if (err) throw err;
 
-        connection.query("SELECT * FROM tbl_music WHERE music_section = 'kids'", function (error, results) {
+        connection.query("SELECT * FROM tbl_music, tbl_movies, tbl_tv WHERE music_section = 'kids' AND tv_section = 'kids'AND movies_section = 'kids'", function (error, results) {
             connection.release();
 
             if (error) throw error;
